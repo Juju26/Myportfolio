74c1b9896bd0c565042fffd08fb67d46c141714e
Juju26 <atshayakumarr26@gmail.com>
2021-11-10 15:04:46 +0530

    db added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
+var showMenu = (toggleId, navId) =>{
+    var toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
     if(toggle && nav){
@@ -12,7 +12,7 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+var navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
   /*Active link*/
@@ -20,13 +20,13 @@ function linkAction(){
   this.classList.add('active');
   
   /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
+  var navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
+var sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
     duration: 2000,
@@ -55,7 +55,3 @@ sr.reveal('.work__img',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
-
-
-
-
